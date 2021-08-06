da36c0a39209a78309982213fef6bc8d0a595d91
ajaydevofficial <ajaydevofficial@gmai.com>
2021-08-07 02:34:23 +0530

    title update

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'High Level',
+    title: 'HighLevel',
     htmlAttrs: {
       lang: 'en'
     },
